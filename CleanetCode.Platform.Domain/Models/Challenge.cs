48fa115215f17bbfb71bdb8928596adf2fe8ecaa
Alex Goolyuk <alex.goolyuk@gmail.com>
2022-04-22 17:27:30 +0300

    created models, controllers

---
+++ CleanetCode.Platform.Domain/Models/Challenge.cs
@@ -0,0 +1,9 @@
+﻿using CleanetCode.Platform.Domain.Interfaces;
+
+namespace CleanetCode.Platform.Domain.Models
+{
+    class Challenge : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
